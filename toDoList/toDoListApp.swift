aaf3ecaf958a82a0cdeb7a24298ef6dccbf5e91a
selinurkmez <selinurkmeez@gmail.com>
2024-08-05 14:14:16 +0300

    Initial Commit

---
+++ toDoList/toDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListApp.swift
+//  toDoList
+//
+//  Created by macbook on 5.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct toDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
